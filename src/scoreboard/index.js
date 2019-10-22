ae0bc86bf4fe3fe7d1f156012d899760ab5ff854
Serene <unexpecte-d@hotmail.com>
2019-10-22 18:59:40 +0800

    Refactor scoreboard

--- src/scoreboard/index.js
+++ src/scoreboard/index.js
@@ -1,12 +1,13 @@
+const SCORE_NAME_MAP = {
+	0: 'love',
+	1: 'fifteen',
+	2: 'thirty',
+	3: 'forty'
+};
+
 function checkScore(playerOneScore = 0, playerTwoScore = 0) {
-	if (playerOneScore === playerTwoScore) {
-		if (playerOneScore === 0) return 'love';
-		if (playerOneScore === 1) return 'fifteen';
-		if (playerOneScore === 2) return 'thirty';
-		if (playerOneScore === 3) return 'forty';
-	}
+	if (playerOneScore === playerTwoScore) return SCORE_NAME_MAP[playerOneScore];
 	return `hello, ${playerOneScore}, ${playerTwoScore}`;
 }
 
 module.exports = { checkScore };
- 
