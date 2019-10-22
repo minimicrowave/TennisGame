f6727839ce2928e78400850a5183acd550e03488
Serene <unexpecte-d@hotmail.com>
2019-10-23 05:57:13 +0800

    Change scoreboard back to class and only saves computed score

--- src/scoreboard/index.js
+++ src/scoreboard/index.js
@@ -5,9 +5,34 @@ const SCORE_NAME_MAP = {
 	3: 'forty'
 };
 
-function checkScore(playerOneScore = 0, playerTwoScore = 0) {
-	if (playerOneScore === playerTwoScore) return SCORE_NAME_MAP[playerOneScore];
-	return `hello, ${playerOneScore}, ${playerTwoScore}`;
+const DEUCE_SCORE = 'deuce';
+
+class ScoreBoard {
+	constructor(playerOneScore, playerTwoScore) {
+		this.currentScore = this.compute(playerOneScore, playerTwoScore);
+	}
+
+	update(playerOneScore, playerTwoScore) {
+        this.currentScore = this.compute(playerOneScore, playerTwoScore);
+    }
+
+	compute(playerOneScore, playerTwoScore) {
+		if (playerOneScore === playerTwoScore) {
+			if (playerOneScore > 2) return DEUCE_SCORE;
+			return `${SCORE_NAME_MAP[playerOneScore]} all`;
+		}
+    }
+    
+    retrieve() {
+        return this.currentScore;
+    }
 }
 
-module.exports = { checkScore };
+// checkScore(playerOneScore = 0, playerTwoScore = 0) {
+// 	if (playerOneScore === playerTwoScore) {
+// 		if (playerOneScore > 2) return DEUCE_SCORE;
+// 		return `${SCORE_NAME_MAP[playerOneScore]} all`;
+// 	}
+// }
+
+module.exports = ScoreBoard;
