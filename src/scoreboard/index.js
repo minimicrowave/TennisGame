a6b7aa60d51a9233141ab361ba3edf5d838e9760
Serene <unexpecte-d@hotmail.com>
2019-10-22 18:59:22 +0800

    Add equal player scores and its tests

--- src/scoreboard/index.js
+++ src/scoreboard/index.js
@@ -1,5 +1,12 @@
 function checkScore(playerOneScore = 0, playerTwoScore = 0) {
+	if (playerOneScore === playerTwoScore) {
+		if (playerOneScore === 0) return 'love';
+		if (playerOneScore === 1) return 'fifteen';
+		if (playerOneScore === 2) return 'thirty';
+		if (playerOneScore === 3) return 'forty';
+	}
 	return `hello, ${playerOneScore}, ${playerTwoScore}`;
 }
 
 module.exports = { checkScore };
+ 
--- test/TennisGame.spec.js
+++ test/TennisGame.spec.js
@@ -9,10 +9,47 @@ describe('TennisGame', () => {
 			expect(() => new TennisGame('c', 10)).to.throw(`Score "c" is not a number.`);
 		});
 	});
-	describe('Check for equal score between players', () => {
-		let tennisGame = new TennisGame();
-		it('Should return "hello, 0, 0" if no input is entered.', () => {
-			expect(tennisGame.tallyScore()).to.equal('hello, 0, 0');
+	describe('.tally()', () => {
+		it('Should return "love" if score is (0, 0).', () => {
+			const tennisGame = new TennisGame();
+			expect(tennisGame.tallyScore()).to.equal('love');
+			expect(tennisGame.tallyScore(0, 0)).to.equal('love');
+		});
+
+		it('Should return "fifteen" if score is (1, 1).', () => {
+			const tennisGame = new TennisGame();
+			const tennisGame1 = new TennisGame(1, 1);
+			tennisGame.addScoreToPlayerOne();
+			tennisGame.addScoreToPlayerTwo();
+
+			expect(tennisGame.tallyScore()).to.equal('fifteen');
+			expect(tennisGame1.tallyScore()).to.equal('fifteen');
+    });
+    
+		it('Should return "thirty" if score is (2, 2).', () => {
+			const tennisGame = new TennisGame();
+			const tennisGame1 = new TennisGame(2, 2);
+			tennisGame.addScoreToPlayerOne();
+			tennisGame.addScoreToPlayerOne();
+			tennisGame.addScoreToPlayerTwo();
+			tennisGame.addScoreToPlayerTwo();
+
+			expect(tennisGame.tallyScore()).to.equal('thirty');
+			expect(tennisGame1.tallyScore()).to.equal('thirty');
+    });
+    
+		it('Should return "thirty" if score is (3, 3).', () => {
+			const tennisGame = new TennisGame();
+			const tennisGame1 = new TennisGame(3, 3);
+			tennisGame.addScoreToPlayerOne();
+			tennisGame.addScoreToPlayerOne();
+			tennisGame.addScoreToPlayerOne();
+			tennisGame.addScoreToPlayerTwo();
+			tennisGame.addScoreToPlayerTwo();
+			tennisGame.addScoreToPlayerTwo();
+
+			expect(tennisGame.tallyScore()).to.equal('forty');
+			expect(tennisGame1.tallyScore()).to.equal('forty');
 		});
 	});
 });
