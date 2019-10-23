1c5b1e85bd51d7fe40fb374315bace9384b35fd2
Serene <unexpecte-d@hotmail.com>
2019-10-23 13:17:53 +0800

    Make scoreboard store player instance

--- src/TennisGame.js
+++ src/TennisGame.js
@@ -3,31 +3,25 @@ const ScoreBoard = require('./scoreboard');
 const Player = require('./player');
 
 class TennisGame {
-	constructor(playerOneScore = 0, playerTwoScore = 0) {
+	constructor(playerOneScore = 0, playerTwoScore = 0, isDeuce = false) {
 		validateScore(playerOneScore);
 		validateScore(playerTwoScore);
 
 		this.playerOne = new Player(playerOneScore);
 		this.playerTwo = new Player(playerTwoScore);
-		this.scoreBoard = new ScoreBoard(playerOneScore, playerTwoScore);
+		this.scoreBoard = new ScoreBoard(this.playerOne, this.playerTwo, isDeuce);
 	}
 
 	addScoreToPlayerOne() {
-        this.playerOne.addScore();
-        this.updateScoreBoard();
-	}
-    
-	addScoreToPlayerTwo() {
-        this.playerTwo.addScore();
-        this.updateScoreBoard();
+		this.playerOne.addScore();
 	}
 
-	updateScoreBoard() {
-		this.scoreBoard.update(this.playerOne.score, this.playerTwo.score);
+	addScoreToPlayerTwo() {
+		this.playerTwo.addScore();
 	}
 
 	tallyScore() {
-		return this.scoreBoard.retrieve();
+		return this.scoreBoard.getScore();
 	}
 }
 
--- src/scoreboard/index.js
+++ src/scoreboard/index.js
@@ -1,3 +1,5 @@
+const { arraySortByAscending } = require('../utils');
+
 const SCORE_NAME_MAP = {
 	0: 'love',
 	1: 'fifteen',
@@ -5,34 +7,64 @@ const SCORE_NAME_MAP = {
 	3: 'forty'
 };
 
-const DEUCE_SCORE = 'deuce';
+const DEUCE_SCORE = 'deuce!';
+
+const ADVANTAGE_SCORE = 'advantage';
+
+const WINNING_SCORE = 4;
+const DEUCE_SCORE_MINIMUM = 3;
+const DEUCE_SCORE_DIFFERENCE = 2;
 
 class ScoreBoard {
-	constructor(playerOneScore, playerTwoScore) {
-		this.currentScore = this.compute(playerOneScore, playerTwoScore);
+	constructor(playerOne, playerTwo, isDeuce) {
+		this.playerOne = playerOne;
+		this.playerTwo = playerTwo;
+		this.isDeuce = isDeuce;
 	}
 
-	update(playerOneScore, playerTwoScore) {
-        this.currentScore = this.compute(playerOneScore, playerTwoScore);
-    }
+	compute() {
+		let playerOneScore = this.playerOne.score;
+		let playerTwoScore = this.playerTwo.score;
 
-	compute(playerOneScore, playerTwoScore) {
-		if (playerOneScore === playerTwoScore) {
-			if (playerOneScore > 2) return DEUCE_SCORE;
-			return `${SCORE_NAME_MAP[playerOneScore]} all`;
+		if (this.isSameScore()) {
+			if (playerOneScore >= DEUCE_SCORE_MINIMUM) {
+				this.isDeuce = true;
+				return DEUCE_SCORE;
+			} else {
+				return `${SCORE_NAME_MAP[playerOneScore]} all`;
+			}
+		} else if (this.isDeuce) {
+			return `${ADVANTAGE_SCORE} Player ${this.hasMoreScore()}`;
+		} else {
+			return `${SCORE_NAME_MAP[playerOneScore]}, ${SCORE_NAME_MAP[playerTwoScore]}`;
 		}
-    }
-    
-    retrieve() {
-        return this.currentScore;
-    }
-}
+	}
 
-// checkScore(playerOneScore = 0, playerTwoScore = 0) {
-// 	if (playerOneScore === playerTwoScore) {
-// 		if (playerOneScore > 2) return DEUCE_SCORE;
-// 		return `${SCORE_NAME_MAP[playerOneScore]} all`;
-// 	}
-// }
+	isWin() {
+		let [ lowerScore, higherScore ] = arraySortByAscending([
+			this.playerOne.score,
+			this.playerTwo.Score
+		]);
+
+		if (higherScore >= WINNING_SCORE && higherScore - lowerScore >= DEUCE_SCORE_DIFFERENCE)
+			return true;
+		return false;
+	}
+
+	isSameScore() {
+		return this.playerOne.score === this.playerTwo.score;
+	}
+
+	hasMoreScore() {
+		return this.playerOne.score > this.playerTwo.score ? 1 : 2;
+	}
+
+	getScore() {
+		if (this.isWin()) {
+			return `Game, Player ${this.hasMoreScore()}`;
+		}
+		return this.compute();
+	}
+}
 
 module.exports = ScoreBoard;
